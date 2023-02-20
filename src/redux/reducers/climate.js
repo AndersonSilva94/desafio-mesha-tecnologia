5a4856a54c3aa66b38c660095ea8215e628347c2
Anderson Silva <andssilva.23@gmail.com>
2023-02-20 00:50:11 -0400

    feat: :rocket: add new states

--- src/redux/reducers/climate.js
+++ src/redux/reducers/climate.js
@@ -2,14 +2,15 @@ import { SET_CLIMATE_LOADING, SET_CLIMATE } from '../actions';
 
 const INITIAL_STATE = {
   climate: {},
+  loadingWeather: false
 };
 
 const climate = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case SET_CLIMATE_LOADING:
-      return { ...state };
+      return { ...state, loadingWeather: true };
     case SET_CLIMATE:
-      return { ...state, climate: { ...action.payload } };
+      return { ...state, climate: { ...action.payload }, loadingWeather: false };
     default:
       return state;
   }
--- src/redux/reducers/playlist.js
+++ src/redux/reducers/playlist.js
@@ -1,15 +1,16 @@
 import { SET_PLAYLIST, SET_PLAYLIST_LOADING } from "../actions";
 
 const INITIAL_STATE = {
-  playlist: {}
+  playlist: {},
+  loadingPlaylist: false
 };
 
 const playlist = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case SET_PLAYLIST_LOADING:
-      return { ...state };
+      return { ...state, loadingPlaylist: true };
     case SET_PLAYLIST:
-      return { ...state, playlist: { ...action.payload }};
+      return { ...state, playlist: { ...action.payload }, loadingPlaylist: false };
     default:
       return state;
   }
